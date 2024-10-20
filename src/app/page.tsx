6786df05813da31f16cdb46cd5e6d707d4d68c1b
Salvatore Avanzo <salvatore.avanzo@gmail.com>
2024-10-20 20:20:05 +0200

    Leaflet integration, Hazards markers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 "use client"
-import MapCanvas from '../components/Map';
+import dynamic from 'next/dynamic';
+import 'leaflet/dist/leaflet.css';
+
+const Map = dynamic(() => import('../components/Map'), {
+  ssr: false, // Evitiamo il rendering lato server
+});
 
 export default function Home() {
   return (
-      <MapCanvas/>
+      <div style={{height: '100vh'}}>
+        <Map/>
+      </div>
   );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,51 @@
-"use client"
-import Map from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 
-const MapCanvas = () => {
+const icon = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOXTOKEN;
+
+const Map: React.FC = () => {
+    const [hazards, setHazards] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.hungermapdata.org/v1/climate/hazards")
+            .then((res) => res.json())
+            .then((data) => {
+                setHazards(data.body.hazards);
+            });
+    }, []);
+
     return (
-        <Map
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOXTOKEN}
-            initialViewState={{
-                longitude: 20,
-                latitude: 0,
-                zoom: 3
-            }}
-            style={{width: '100%', height: '800px'}}
-            mapStyle="mapbox://styles/mapbox/dark-v10"
-        />
+        <MapContainer
+            center={[0, 20]}
+            zoom={2}
+            style={{ height: '100vh', width: '100%' }}
+        >
+            <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=${MAPBOX_ACCESS_TOKEN}`}
+                attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> contributors'
+                tileSize={512}
+                zoomOffset={-1}
+            />
+            {hazards && hazards.map((hazard) => (
+                <Marker key={hazard.id} position={[hazard.latitude, hazard.longitude]} icon={icon}>
+                    <Popup>{hazard.name}</Popup>
+                </Marker>
+            ))}
+        </MapContainer>
     );
-}
+};
 
-export default MapCanvas;
+export default Map;
+
+
+
+
